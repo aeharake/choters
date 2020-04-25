824d080667bd4d1db4995f6dbc4dc252b970c277
Ahmad El Harake <aeharake@gmail.com>
2020-04-25 23:00:23 +0300

    Implemented random name generation and unit tested the size

--- app/src/test/java/com/aeharake/choters/ExampleUnitTest.kt
+++ app/src/test/java/com/aeharake/choters/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package com.aeharake.choters
 
+import com.aeharake.choters.mocker.UsersGenerator
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -13,5 +14,10 @@ class ExampleUnitTest {
   @Test
   fun addition_isCorrect() {
     assertEquals(4, 2 + 2)
+  }
+
+  @Test
+  fun test_names_size(){
+    assertEquals(200, UsersGenerator.getRandomFullNames().size)
   }
 }
