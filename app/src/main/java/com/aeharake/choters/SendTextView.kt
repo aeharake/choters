52e4f79ac398d3ee49900fa1a9a0603fbcf23da5
Ahmad El Harake <aeharake@gmail.com>
2020-04-25 22:17:15 +0300

    Implemented conversation layout with custom SendTextView

---
+++ app/src/main/java/com/aeharake/choters/SendTextView.kt
@@ -0,0 +1,42 @@
+package com.aeharake.choters
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.EditText
+import android.widget.FrameLayout
+import android.widget.ImageView
+
+class SendTextView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    private var etText: EditText
+    private var btnSend: ImageView
+
+    interface OnSendClickListener{
+        fun onSend(text: String)
+    }
+    private var onSendClickListener: OnSendClickListener? = null
+    init {
+        inflate(context, R.layout.send_text_layout, this)
+        etText = findViewById(R.id.et_text)
+        btnSend = findViewById(R.id.btn_send)
+        btnSend.setOnClickListener {
+            onSendClickListener?.onSend(getText())
+            clearText()
+        }
+    }
+
+    private fun clearText() {
+        etText.setText("")
+    }
+
+    fun getText() : String{
+        return etText.text.toString()
+    }
+
+    fun setOnSendClickListener(onSendClickListener: OnSendClickListener?){
+        this.onSendClickListener = onSendClickListener
+    }
+
+}
