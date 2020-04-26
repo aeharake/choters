e5a71f5f628ab8ee3c8da23f21ab2c804a83817d
Ahmad El Harake <aeharake@gmail.com>
2020-04-26 08:39:15 +0300

    Final ConversationViewModel implemented

--- app/src/main/java/com/aeharake/choters/ConversationViewModel.kt
+++ app/src/main/java/com/aeharake/choters/ConversationViewModel.kt
@@ -2,7 +2,21 @@ package com.aeharake.choters
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.aeharake.choters.repos.MessageRepository
+import com.aeharake.choters.room.entities.Message
 
 class ConversationViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository: MessageRepository = MessageRepository(application)
+
+    fun getAllMessage(id: Int): LiveData<List<Message>> {
+        return repository.getAllMessages(id)
+    }
+
+    fun insertAndEcho(text: String,id: Int){
+        repository.insertMessageAndEcho(text, id)
+
+        //we shouldn't echo anything here, because insert is async, so we should insert and echo in background
+    }
 
 }
