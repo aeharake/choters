359ac88a926531980a28de3455dfef70fce25852
Ahmad El Harake <aeharake@gmail.com>
2020-04-26 10:21:39 +0300

    Do not allow empty text to be sent

--- app/src/main/java/com/aeharake/choters/SendTextView.kt
+++ app/src/main/java/com/aeharake/choters/SendTextView.kt
@@ -1,10 +1,13 @@
 package com.aeharake.choters
 
 import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.AttributeSet
 import android.widget.EditText
 import android.widget.FrameLayout
 import android.widget.ImageView
+import androidx.core.widget.addTextChangedListener
 
 class SendTextView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
@@ -13,29 +16,48 @@ class SendTextView @JvmOverloads constructor(
     private var etText: EditText
     private var btnSend: ImageView
 
-    interface OnSendClickListener{
+    interface OnSendClickListener {
         fun onSendClick(text: String)
     }
+
     private var onSendClickListener: OnSendClickListener? = null
+
     init {
         inflate(context, R.layout.send_text_layout, this)
         etText = findViewById(R.id.et_text)
         btnSend = findViewById(R.id.btn_send)
+        etText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                btnSend.isEnabled = s.toString().isNotBlank()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+
+        })
         btnSend.setOnClickListener {
             onSendClickListener?.onSendClick(getText())
             clearText()
         }
+        btnSend.isEnabled = false
     }
+
 
     private fun clearText() {
         etText.setText("")
     }
 
-    fun getText() : String{
+    fun getText(): String {
         return etText.text.toString()
     }
 
-    fun setOnSendClickListener(onSendClickListener: OnSendClickListener?){
+    fun setOnSendClickListener(onSendClickListener: OnSendClickListener?) {
         this.onSendClickListener = onSendClickListener
     }
 
