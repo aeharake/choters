de1ece15165e9ac1ab21ad997ec6532cfaa2c8d5
Ahmad El Harake <aeharake@gmail.com>
2020-04-26 08:33:35 +0300

    Added Timber library for logging

--- app/src/main/java/com/aeharake/choters/ApplicationController.kt
+++ app/src/main/java/com/aeharake/choters/ApplicationController.kt
@@ -2,11 +2,15 @@ package com.aeharake.choters
 
 import android.app.Application
 import com.facebook.drawee.backends.pipeline.Fresco
+import timber.log.Timber
 
 class ApplicationController : Application() {
 
     override fun onCreate() {
         super.onCreate()
         Fresco.initialize(this);
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
