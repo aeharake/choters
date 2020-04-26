645b5c1aa36bceeb914b290a26cd45bf8a2e79c0
Ahmad El Harake <aeharake@gmail.com>
2020-04-26 03:35:45 +0300

    Removed useless code in UsersVieWModel

--- app/src/main/java/com/aeharake/choters/UsersViewModel.kt
+++ app/src/main/java/com/aeharake/choters/UsersViewModel.kt
@@ -7,12 +7,8 @@ import com.aeharake.choters.repos.UserRepository
 import com.aeharake.choters.room.entities.User
 
 class UsersViewModel(application: Application) : AndroidViewModel(application) {
-    var usersRepository: UserRepository = UserRepository(application)
+    private var usersRepository: UserRepository = UserRepository(application)
     val users: LiveData<List<User>> = usersRepository.getAllUsers()
-
-    fun addUser(user: User) {
-        usersRepository.insertUsers(user)
-    }
 
     fun populate(){
         usersRepository.populate()
